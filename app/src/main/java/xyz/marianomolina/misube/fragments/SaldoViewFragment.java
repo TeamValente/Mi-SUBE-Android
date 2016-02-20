412f97a971b8e76e4e4b0c3fd854cf6786bfc8df
Mariano Molina <xsin.crueldadx@gmail.com>
2016-02-20 18:37:12 -0300

    :lipstick: Add layout para la vista de movimientos

--- app/src/main/java/xyz/marianomolina/misube/fragments/SaldoViewFragment.java
+++ app/src/main/java/xyz/marianomolina/misube/fragments/SaldoViewFragment.java
@@ -1,11 +1,15 @@
 package xyz.marianomolina.misube.fragments;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
+import xyz.marianomolina.misube.MovimientosActivity;
 import xyz.marianomolina.misube.R;
 
 
@@ -23,5 +27,19 @@ public class SaldoViewFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_saldo_view, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+
+        Button btn_open_movimientos = (Button) getActivity().findViewById(R.id.btn_open_move_list);
+        btn_open_movimientos.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intentMovimientos = new Intent(getContext(), MovimientosActivity.class);
+                getActivity().startActivity(intentMovimientos);
+            }
+        });
     }
 }
