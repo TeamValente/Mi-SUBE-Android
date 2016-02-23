03557e542cc79e1d6d80ecac86edb7d1700f9766
Mariano Molina <xsin.crueldadx@gmail.com>
2016-02-22 23:16:00 -0300

    :rocket: GMaps funcionando

--- app/src/main/java/xyz/marianomolina/misube/fragments/MapViewFragment.java
+++ app/src/main/java/xyz/marianomolina/misube/fragments/MapViewFragment.java
@@ -28,7 +28,6 @@ public class MapViewFragment extends Fragment {
     // TAG
     private static final String LOG_TAG = MapViewFragment.class.getSimpleName();
 
-    private SupportMapFragment supportMapFragment;
     private GoogleMap map;
 
     public MapViewFragment() {
@@ -40,7 +39,7 @@ public class MapViewFragment extends Fragment {
         View rootView =  inflater.inflate(R.layout.fragment_map_view, container, false);
 
         FragmentManager fm = getChildFragmentManager();
-        supportMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+        SupportMapFragment supportMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
 
         if (supportMapFragment == null) {
             supportMapFragment = SupportMapFragment.newInstance();
@@ -73,89 +72,4 @@ public class MapViewFragment extends Fragment {
 
         return rootView;
     }
-
-    /**
-     *  Posible respuesta al bug.
-     *  http://stackoverflow.com/questions/18206615/how-to-use-google-map-v2-inside-fragment#answer-20721742
-     * */
-/*
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-
-        FragmentManager fm = getChildFragmentManager();
-        supportMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
-
-        if (supportMapFragment == null) {
-            supportMapFragment = SupportMapFragment.newInstance();
-            fm.beginTransaction().replace(R.id.map_container, supportMapFragment).commit();
-        }
-
-        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
-            @Override
-            public void onMapReady(GoogleMap googleMap) {
-                map = googleMap;
-
-                map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                    // TODO: Consider calling
-                    //    ActivityCompat#requestPermissions
-                    // here to request the missing permissions, and then overriding
-                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                    //                                          int[] grantResults)
-                    // to handle the case where the user grants the permission. See the documentation
-                    // for ActivityCompat#requestPermissions for more details.
-                    return;
-                }
-                map.setMyLocationEnabled(true);
-                // Centramos el mapa en BUENOS AIRES
-                LatLng BUE = new LatLng(-34.6160275, -58.4333203);
-                map.moveCamera(CameraUpdateFactory.newLatLngZoom(BUE, 13));
-
-            }
-        });
-
-
-    }
-
-*/
-
-/*
-    @Override
-    public void onResume() {
-        super.onResume();
-        if (map == null) {
-
-            map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
-            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
-                // TODO: Consider calling
-                //    ActivityCompat#requestPermissions
-                // here to request the missing permissions, and then overriding
-                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
-                //                                          int[] grantResults)
-                // to handle the case where the user grants the permission. See the documentation
-                // for ActivityCompat#requestPermissions for more details.
-                return;
-            }
-            map.setMyLocationEnabled(true);
-            // Centramos el mapa en BUENOS AIRES
-            LatLng BUE = new LatLng(-34.6160275, -58.4333203);
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(BUE, 13));
-        }
-    }*/
-
-/*    @TargetApi(Build.VERSION_CODES.KITKAT)
-    @Override
-    public void onDetach() {
-        super.onDetach();
-
-        try {
-            Field childFragmentManager = Fragment.class.getDeclaredField("mChildFragmentManager");
-            childFragmentManager.setAccessible(true);
-            childFragmentManager.set(this, null);
-        } catch (NoSuchFieldException | IllegalAccessException e) {
-            e.printStackTrace();
-        }
-
-    }*/
 }
