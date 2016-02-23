7a88d0ec5601b1c6535ddfd17bdd408794b3f314
Mariano Molina <xsin.crueldadx@gmail.com>
2016-02-22 22:37:25 -0300

    :bug: Error al inicilizar el Map. :lipstick: Actualización de dependencias

--- app/src/main/java/xyz/marianomolina/misube/fragments/MapViewFragment.java
+++ app/src/main/java/xyz/marianomolina/misube/fragments/MapViewFragment.java
@@ -2,11 +2,8 @@ package xyz.marianomolina.misube.fragments;
 
 
 import android.Manifest;
-import android.annotation.TargetApi;
 import android.content.pm.PackageManager;
-import android.os.Build;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
@@ -16,10 +13,9 @@ import android.view.ViewGroup;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
-
-import java.lang.reflect.Field;
 
 import xyz.marianomolina.misube.R;
 
@@ -41,30 +37,95 @@ public class MapViewFragment extends Fragment {
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_map_view, container, false);
+        View rootView =  inflater.inflate(R.layout.fragment_map_view, container, false);
+
+        FragmentManager fm = getChildFragmentManager();
+        supportMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+
+        if (supportMapFragment == null) {
+            supportMapFragment = SupportMapFragment.newInstance();
+            fm.beginTransaction().replace(R.id.map_container, supportMapFragment).commit();
+        }
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                map.setMyLocationEnabled(true);
+                // Centramos el mapa en BUENOS AIRES
+                LatLng BUE = new LatLng(-34.6160275, -58.4333203);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(BUE, 13));
+
+            }
+        });
+
+        return rootView;
     }
 
     /**
      *  Posible respuesta al bug.
      *  http://stackoverflow.com/questions/18206615/how-to-use-google-map-v2-inside-fragment#answer-20721742
      * */
+/*
     @Override
     public void onActivityCreated(@Nullable Bundle savedInstanceState) {
         super.onActivityCreated(savedInstanceState);
 
         FragmentManager fm = getChildFragmentManager();
         supportMapFragment = (SupportMapFragment) fm.findFragmentById(R.id.map_container);
+
         if (supportMapFragment == null) {
             supportMapFragment = SupportMapFragment.newInstance();
             fm.beginTransaction().replace(R.id.map_container, supportMapFragment).commit();
         }
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                map = googleMap;
+
+                map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+                if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                    // TODO: Consider calling
+                    //    ActivityCompat#requestPermissions
+                    // here to request the missing permissions, and then overriding
+                    //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                    //                                          int[] grantResults)
+                    // to handle the case where the user grants the permission. See the documentation
+                    // for ActivityCompat#requestPermissions for more details.
+                    return;
+                }
+                map.setMyLocationEnabled(true);
+                // Centramos el mapa en BUENOS AIRES
+                LatLng BUE = new LatLng(-34.6160275, -58.4333203);
+                map.moveCamera(CameraUpdateFactory.newLatLngZoom(BUE, 13));
+
+            }
+        });
+
+
     }
 
+*/
+
+/*
     @Override
     public void onResume() {
         super.onResume();
         if (map == null) {
-            map = supportMapFragment.getMap();
+
             map.setMapType(GoogleMap.MAP_TYPE_NORMAL);
             if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
                 // TODO: Consider calling
@@ -81,9 +142,9 @@ public class MapViewFragment extends Fragment {
             LatLng BUE = new LatLng(-34.6160275, -58.4333203);
             map.moveCamera(CameraUpdateFactory.newLatLngZoom(BUE, 13));
         }
-    }
+    }*/
 
-    @TargetApi(Build.VERSION_CODES.KITKAT)
+/*    @TargetApi(Build.VERSION_CODES.KITKAT)
     @Override
     public void onDetach() {
         super.onDetach();
@@ -96,5 +157,5 @@ public class MapViewFragment extends Fragment {
             e.printStackTrace();
         }
 
-    }
+    }*/
 }
