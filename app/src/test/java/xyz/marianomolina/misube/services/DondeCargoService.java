c688a75013a9b192bd077bb37c1e05bc51d41f89
Mariano Molina <xsin.crueldadx@gmail.com>
2016-02-29 22:02:05 -0300

    Add services package

---
+++ app/src/test/java/xyz/marianomolina/misube/services/DondeCargoService.java
@@ -0,0 +1,17 @@
+package xyz.marianomolina.misube.services;
+
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
+/**
+ * Created by hernancoppola on 29/2/16.
+ */
+public class DondeCargoService {
+
+
+    public String generarURLValida(String url) throws UnsupportedEncodingException {
+        //Devuelve una url valida
+        return URLEncoder.encode(url,"UTF-8");
+    }
+
+}
