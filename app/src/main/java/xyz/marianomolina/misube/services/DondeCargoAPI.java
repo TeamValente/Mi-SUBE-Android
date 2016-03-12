8ac6bd50814a8fd67f94ede3422da0f56ddce600
Hernan Matias Coppola <hernan.coppola@gmail.com>
2016-03-12 17:47:53 -0300

    Arreglo de la API para que traiga los puntos correctos. Se cambio el Zoom del mapa para que no muestre todos los puntos amontonados.

--- app/src/main/java/xyz/marianomolina/misube/services/DondeCargoAPI.java
+++ app/src/main/java/xyz/marianomolina/misube/services/DondeCargoAPI.java
@@ -7,7 +7,9 @@ import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 import retrofit2.http.Field;
 import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Query;
 import xyz.marianomolina.misube.model.PuntoCarga;
 
 /**
@@ -17,7 +19,7 @@ public interface DondeCargoAPI {
 
     @FormUrlEncoded
     @POST("core/?query=getNearPoints")
-    Call<List<PuntoCarga>> loadPuntosCarga(@Field("session") String session, @Field("lat") double lat, @Field("lng") double lng);
+    Call<List<PuntoCarga>> loadPuntosCarga(@Field("session") String session, @Field("params[lat]") double lat, @Field("params[lng]") double lng);
 
     public static final Retrofit retrofit = new Retrofit.Builder()
             .baseUrl("http://dondecargolasube.com.ar/")
